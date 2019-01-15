df22f0e114fa6742f795579f6e452761846c140c
Patryk <patchwialkowski.com>
2019-01-15 19:16:58 +0100

    coś

---
+++ script.js
@@ -0,0 +1,21 @@
+//let age = 20
+//function showAge(v) {
+//    return `I'm ${v + 5} y.o`
+//}
+//showAge(age)
+//let age = 20
+//function calculater(x) {
+//    return `${x + 10}`
+//    
+//}
+//calculater(age)
+
+
+function count(){
+    var x = document.querySelector("#txt1").value;
+    var y = document.querySelector("#txt2").value;
+    var z = Number(x) + Number(y);
+    document.querySelector("#result").innerHTML = z;
+    
+}
+
